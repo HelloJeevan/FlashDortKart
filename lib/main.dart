e90ac68574afbda18fd2265d26d21f83389c7e4c
Jeevan gautam <jeevan.gautam.e21@nsut.ac.in>
2022-05-27 11:19:28 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basic/Pages/HomePage.dart';
+import 'package:flutter_basic/Pages/LoginPage.dart';
+import 'package:google_fonts/google_fonts.dart';
+void main(){
+  runApp(MyApp() );
+}
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //home: HomePage(),
+      themeMode:ThemeMode.light ,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        //primaryTextTheme: GoogleFonts.latoTextTheme()
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      routes: {
+        "/":(context) => LoginPage(),
+        "/home":(context) => HomePage(),
+
+      },
+    );
+  }
+}
